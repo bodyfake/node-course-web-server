80f1d48bb5fe5e098f13310643edaeda1e435d5f
Julian Steffen <james14@mail.de>
2017-11-21 23:29:20 +0100

    Init Commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+const app = express();
+
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFileSync('server.log', path.join(log, '\n'), (err) => {
+    if (err) {
+      console.log('Unable to appen to server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintaince.hbs');
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'MessageError',
+  });
+});
+app.listen(3000, () => {
+  console.log('Server is up on Port 3000');
+});
